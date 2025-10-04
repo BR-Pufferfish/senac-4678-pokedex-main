67b673b41ad30f8c1e0c519c9d5e99f85768f6cc
Puffer Notebook <marceloballinflores@gmail.com>
2025-10-03 21:52:42 -0300

    first commit

---
+++ pokemon/script.js
@@ -0,0 +1,45 @@
+
+function getPokemon(){
+    const infos = JSON.parse(localStorage.getItem("pokemon"))
+    console.log(infos, "pokemon")
+    const div = document.querySelector("div")
+    div.classList.add("card")
+    
+
+    //name
+    const p = document.createElement("p")
+    p.innerText = infos.name
+    p.classList.add("name")
+
+    //image
+    const img = document.createElement("img")
+    img.src = infos.sprites.front_default
+    
+    //expeirence
+    const experience = document.createElement("p")
+    experience.innerText ="Base experience: " + infos.base_experience
+
+    //abilities
+    const abilities = document.createElement("div")
+    const abilityTitle = document.createElement("p")
+    abilityTitle.innerText = "Abilities"
+    abilities.append(abilityTitle)
+    infos.abilities.forEach((item)=>{
+        const ability = document.createElement("p")
+        ability.innerText = item.ability.name
+        abilities.append(ability)
+    })
+
+    //type
+    const type = document.createElement("p")
+    type.innerText = "Type: " + infos.types[0].type.name
+
+    
+
+
+    
+    
+    div.append(img,p,type,experience,abilities)
+
+}
+getPokemon()
---
+++ scrip.js
@@ -0,0 +1,47 @@
+const typesClasses = {
+    normal: "normal",
+    fire: "fire",
+    water: "water",
+    grass: "grass",
+    electric: "electric",
+    ice: "ice",
+    fighting: "fighting",
+    poison: "poison",
+    ground: "ground",
+    flying: "flying",
+    psychic: "psychic",
+    bug: "bug",
+    rock: "rock",
+    ghost: "ghost",
+    dragon: "dragon",
+    dark: "dark",
+    steel: "steel",
+    fairy: "fairy"
+};
+async function getPokemons(){
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    const pokemons = await response.json()
+    const ul = document.querySelector("ul")
+    pokemons.results.forEach(async(pokemon)=>{
+
+        const li = document.createElement("li")
+       
+        const p = document.createElement("p")
+        p.innerText = pokemon.name
+
+        const response = await fetch(pokemon.url)
+        const infos = await response.json()
+        console.log(infos,"infos")
+        li.classList.add(typesClasses[infos.types[0].type.name])
+         li.addEventListener("click",()=>{
+            localStorage.setItem("pokemon",JSON.stringify(infos))
+            location.href = "/pokemon"
+        })
+        const img = document.createElement("img")
+        img.src = infos.sprites.front_default
+        
+        li.append(img,p)
+        ul.append(li)
+    })
+}
+getPokemons()
