43afd131f3b8474a0bf9d0b75246b278751d8e60
PufferDesktop <marceloballinflores@gmail.com>
2025-10-04 17:11:04 -0300

    ajustes

--- pokemon/script.js
+++ pokemon/script.js
@@ -25,38 +25,19 @@ function getPokemon(){
     div.classList.add("card")
     div.classList.add(typesClasses[infos.types[0].type.name])
 
-    //name
     const p = document.createElement("p")
     p.innerText = `${infos.id} - ${infos.name}`
     p.classList.add("name")
 
-    //image
     const img = document.createElement("img")
     img.src = infos.sprites.front_default
-    
-    // //experience
-    // const experience = document.createElement("p")
-    // experience.innerText =`Exp. Base: ${infos.base_experience}`
-
-    // //abilities
-    // const abilities = document.createElement("div")
-    // const abilityTitle = document.createElement("p")
-    // abilityTitle.innerText = "Habilidades:"
-    // abilities.append(abilityTitle)
-    // infos.abilities.forEach((item)=>{
-    //     const ability = document.createElement("p")
-    //     ability.innerText = item.ability.name
-    //     abilities.append(ability)
-    // })
-
-    // //type
-    // const type = document.createElement("p")
-    // type.innerText = `Tipo: ${infos.types[0].type.name} ${infos.types[1] ? " / "+infos.types[1].type.name : ""}`
 
     div.append(img,p)
 
 }
 getPokemon()
+
+
 
 function getInfos(){
     const infos = JSON.parse(localStorage.getItem("pokemon"))
@@ -66,11 +47,9 @@ function getInfos(){
     divGeral.classList.add("geral")
     divGeral.classList.add(typesClasses[infos.types[0].type.name])
 
-    //experience
     const experience = document.createElement("p")
     experience.innerText =`Exp. Base: ${infos.base_experience}`
 
-    //abilities
     const abilities = document.createElement("div")
     const abilityTitle = document.createElement("p")
     abilityTitle.innerText = "Habilidades:"
@@ -78,17 +57,82 @@ function getInfos(){
     infos.abilities.forEach((item)=>{
         const ability = document.createElement("p")
         ability.innerText = item.ability.name
-        abilities.append(ability)
+        const span = document.createElement("span")
+        span.append(ability)
+        abilities.append(span)
     })
 
-    //type
     const type = document.createElement("p")
     type.innerText = `Tipo: ${infos.types[0].type.name} ${infos.types[1] ? " / "+infos.types[1].type.name : ""}`
 
     divGeral.append(experience,type,abilities)
     document.body.append(divGeral)
 
+}
+getInfos()
+
+
+
+function getMoves(){
+
+    const button = document.createElement("button")
+    button.innerText = "Ver Movimentos"
+    button.classList.add("btn-moves")
+    document.body.append(button)
+    button.addEventListener("click", ()=>{
+
+        button.style.display = "none"
+        const infos = JSON.parse(localStorage.getItem("pokemon"))
+        const divMoves = document.createElement("div")
+        divMoves.classList.add("moves")
+        divMoves.classList.add(typesClasses[infos.types[0].type.name])
+        const movesTitle = document.createElement("p")
+        movesTitle.innerText = "Movimentos: "
+        divMoves.append(movesTitle)
+        let count = 0;
+        infos.moves.forEach((item)=>{
+            count++
+            const move = document.createElement("p")
+            move.innerText = `${count} - ${item.move.name}`
+            const span = document.createElement("span")
+            span.append(move)
+            divMoves.append(span)
+        })
+        document.body.append(divMoves)
+
+    })
+
+    const buttonHide = document.createElement("button")
+    buttonHide.innerText = "Ocultar Movimentos"
+    buttonHide.classList.add("btn-moves")
+    document.body.append(buttonHide)
+
+    buttonHide.addEventListener("click", ()=>{
+        button.style.display = "block"
+        const divMoves = document.querySelector(".moves")
+        divMoves.remove()
+    })
 
 
 }
-getInfos()
+getMoves()
+
+
+function getStats(){
+    const infos = JSON.parse(localStorage.getItem("pokemon"))
+    const divStats = document.createElement("div")
+    divStats.classList.add("stats")
+    divStats.classList.add(typesClasses[infos.types[0].type.name])
+    const statsTitle = document.createElement("p")
+    statsTitle.innerText = "Status: "
+    divStats.append(statsTitle)
+    infos.stats.forEach((item)=>{
+        const stat = document.createElement("p")
+        stat.innerText = `${item.stat.name}: ${item.base_stat}`
+        const span = document.createElement("span")
+        span.append(stat)
+        divStats.append(span)
+    })
+    document.body.append(divStats)
+}
+getStats()
